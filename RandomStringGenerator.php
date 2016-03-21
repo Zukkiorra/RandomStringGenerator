140ccaa9d5c98a8dffd652996fcdc98f335eae7c
Aleksandar <zukkiorra@gmail.com>
2016-03-21 20:00:26 +0100

    Added random stirng generation mechanism.

--- RandomStringGenerator.php
+++ RandomStringGenerator.php
@@ -2,5 +2,19 @@
 
 class RandomStringGenerator
 {
-	
+	private $generatedStirng = "";
+	private $characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+=/;:(){}[]!@#$%^&*-_|?<>";
+
+	public function setRandomString($length=10)
+	{
+		$numOfCharacters = strlen($this->characters);
+		for ($i=0; $i < $length; $i++) { 
+			$this->generatedStirng .= $this->characters[rand(0, $numOfCharacters-1)];
+		}
+	}
+
+	public function getRandomString()
+	{
+		return $this->generatedStirng;
+	}
 }
